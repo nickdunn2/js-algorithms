ae9b9ea03f185884e1d96c382c23c43ff99b15f2
Nick Dunn <nick.dunn@flosports.tv>
2020-04-28 09:34:37 -0500

    add functions (with timing)

---
+++ Add2_Timing/index.js
@@ -0,0 +1,10 @@
+const { performance } = require('perf_hooks')
+
+const addUpTo = (n) => {
+    return n * (n + 1) / 2
+}
+
+const t1 = performance.now()
+addUpTo(1000000000)
+const t2 = performance.now()
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
