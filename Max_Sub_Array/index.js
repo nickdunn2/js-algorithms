19d6a0ab56e1d64e9c8341afb73288d9db6144b4
Nick Dunn <nick.dunn@flosports.tv>
2020-11-11 10:23:11 -0600

    add Max_Sub_Array (Sliding Window) algorithm

---
+++ Max_Sub_Array/index.js
@@ -0,0 +1,35 @@
+/**
+ * A function that accepts an array of positive ints and a positive number num.
+ * Calculates the maximum sum of n consecutive elements in the array.
+ * 
+ * (Uses the Sliding Window Pattern to avoid nested loops.)
+ */
+function maxSubArraySum(arr, num) {
+    // Set up initial variables. 
+    let maxSum = 0
+    let tempSum = 0
+
+    // Return null if num is larger than the array provided.
+    if (arr.length < num) return null
+
+    // Sum up the first num amount of integers (initial "window") to create starting maxSum.
+    for (let i = 0; i < num; i++) {
+        maxSum += arr[i]
+    }
+
+    tempSum = maxSum
+
+    // Starting at num, subtract the trailing value of the "window" and replace with the next value.
+    // Compare tempSum to existing maxSum each time and reset if necessary.
+    for (let i = num; i < arr.length; i++) {
+        tempSum = tempSum - arr[i - num] + arr[i]
+        maxSum = Math.max(maxSum, tempSum)
+    }
+
+    return maxSum
+}
+
+console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 3)) // 19
+console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 4)) // 22
+console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 15)) // null
+console.log(maxSubArraySum([2,6,9], 3)) // 17
