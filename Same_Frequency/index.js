98cedefccb5b4533ba58945bc302d01880ba8b6a
Nick Dunn <nick.dunn@flosports.tv>
2020-11-21 12:40:19 -0600

    add some tweaks to Same_Frequency algorithm

--- Same_Frequency/index.js
+++ Same_Frequency/index.js
@@ -4,6 +4,10 @@
  * Your solution must have O(n) time complexity. (Hint: Use Frequency Counter Pattern.)
  */
 function sameFrequency(num1, num2) {
+    if (!passesAllConditions(num1, num2)) {
+        return false
+    }
+
     const obj1 = buildDictionaryFromString(num1.toString())
     const obj2 = buildDictionaryFromString(num2.toString())
 
@@ -14,6 +18,10 @@ function sameFrequency(num1, num2) {
     }
 
     return true
+}
+
+function passesAllConditions(num1, num2) {
+    return num1 > 0 && num2 > 0 && (num1.toString().length === num2.toString().length)
 }
 
 function buildDictionaryFromString(str) {
@@ -36,4 +44,5 @@ console.log(sameFrequency(182, 281)) // true
 console.log(sameFrequency(34, 14)) // false
 console.log(sameFrequency(22, 222)) // false
 console.log(sameFrequency(3589578, 5879385)) // true
-
+console.log(sameFrequency(-40, 40)) // false
+console.log(sameFrequency(424, 42424242424)) // false
