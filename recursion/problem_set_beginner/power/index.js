99240f4995a7a406e63610230c2af79b8d034dee
Nick Dunn <nick.dunn@flosports.tv>
2021-09-09 09:22:13 -0500

    feat: introduce recursion solution for power

---
+++ recursion/problem_set_beginner/power/index.js
@@ -0,0 +1,16 @@
+const power = (base, exponent) => {
+  if (exponent <= 0) { // not accounting properly for negative exponents :shrug:
+    return 1
+  }
+
+  if (exponent === 1) {
+    return base
+  }
+
+  return base * power(base, exponent - 1)
+}
+
+console.log(power(2, 4)) // 16
+console.log(power(2, 0)) // 1
+console.log(power(2, 2)) // 4
+console.log(power(4, 3)) // 64
