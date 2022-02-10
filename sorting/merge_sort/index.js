305288a9067991c0879d56441b329dc9f99ac0a0
Nick Dunn <nick.dunn@flosports.tv>
2022-02-10 09:21:01 -0600

    feat: introduce mergeSort algorithm

--- sorting/merge_sort/index.js
+++ sorting/merge_sort/index.js
@@ -1,5 +1,19 @@
 /**
+ * Function using a mergeSort algorithm to sort an unsorted array (of integers).
+ */
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr
+
+  let mid = Math.floor(arr.length / 2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+
+  return mergeArrays(left, right)
+}
+
+/**
  * Function to merge two sorted arrays (without using built-in .sort())
+ * This is vital for any mergeSort algorithm.
  */
 const mergeArrays = (arr1, arr2) => {
   let i = 0
@@ -32,6 +46,8 @@ const mergeArrays = (arr1, arr2) => {
   return merged
 }
 
-console.log(mergeArrays([1,10,50], [2,14,99,100])) // [1,2,4,10,50,99,100]
-console.log(mergeArrays([], [2,4,8])) // [2,4,8]
-console.log(mergeArrays([100,200,9000], [])) // [100,200,9000]
+console.log(mergeSort([10,24,76,73,72,1,9])) // [1,9,10,24,72,73,76]
+
+// console.log(mergeArrays([1,10,50], [2,14,99,100])) // [1,2,4,10,50,99,100]
+// console.log(mergeArrays([], [2,4,8])) // [2,4,8]
+// console.log(mergeArrays([100,200,9000], [])) // [100,200,9000]
