631a1eb6a95e122c3c0612514777e98789a50ca0
Nick Dunn <nick.dunn@flosports.tv>
2022-04-19 09:27:31 -0500

    feat(radix-sort): introduce digitCount() helper method

--- sorting/radix_sort/index.js
+++ sorting/radix_sort/index.js
@@ -14,3 +14,19 @@ console.log(getDigit(7323,3)) // 7
 console.log(getDigit(7323,2)) // 3
 console.log(getDigit(7323,1)) // 2
 console.log(getDigit(7323,0)) // 3
+
+/**
+ * A helper function to return the number of digits in a (base 10) number.
+ *
+ * digitCount(1) // 1
+ * digitCount(25) // 2
+ * digitCount(314) // 3
+ */
+const digitCount = (num) => {
+  if (num === 0) return 1
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+console.log(digitCount(1)) // 1
+console.log(digitCount(25)) // 2
+console.log(digitCount(314)) // 3
