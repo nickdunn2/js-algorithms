bf085fde6d8f58d4ae92c3d0502f015008d78627
Nick Dunn <nick.dunn@flosports.tv>
2022-11-07 08:12:59 -0600

    refactor(hash): slight improvements to hash function

--- hash/index.js
+++ hash/index.js
@@ -3,15 +3,17 @@
  */
 const hash = (key, arrayLength) => {
   let total = 0
-  for (let char of key) {
+  let WEIRD_PRIME = 31
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i]
     // map "a" to 1, "b" to 2, "c" to 3, etc.
     let value = char.charCodeAt(0) - 96
-    total = (total + value) % arrayLength
+    total = (total + WEIRD_PRIME + value) % arrayLength
   }
 
   return total
 }
 
-console.log(hash("pink", 10)) // 0
-console.log(hash("orangered", 10)) // 7
-console.log(hash("cyan", 10)) // 3
+console.log(hash("pink", 10)) // 4
+console.log(hash("orangered", 10)) // 6
+console.log(hash("cyan", 10)) // 7
