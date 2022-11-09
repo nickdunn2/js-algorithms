66319f017494f9454c5c0ddb9b6ee617119f376d
Nick Dunn <nick.dunn@flosports.tv>
2022-11-09 09:36:52 -0600

    refactor(hash): change to basic HashTable class

--- hash/index.js
+++ hash/index.js
@@ -1,19 +1,26 @@
-/**
- * Basic hash function that works on strings only.
- */
-const hash = (key, arrayLength) => {
-  let total = 0
-  let WEIRD_PRIME = 31
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i]
-    // map "a" to 1, "b" to 2, "c" to 3, etc.
-    let value = char.charCodeAt(0) - 96
-    total = (total + WEIRD_PRIME + value) % arrayLength
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size)
   }
 
-  return total
+  /**
+   * Basic hash function that works on strings only.
+   */
+  _hash(key) {
+    let total = 0
+    let WEIRD_PRIME = 31
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i]
+      // map "a" to 1, "b" to 2, "c" to 3, etc.
+      let value = char.charCodeAt(0) - 96
+      total = (total + WEIRD_PRIME + value) % this.keyMap.length
+    }
+
+    return total
+  }
 }
 
-console.log(hash("pink", 10)) // 4
-console.log(hash("orangered", 10)) // 6
-console.log(hash("cyan", 10)) // 7
+// console.log(hash("pink", 10)) // 4
+// console.log(hash("orangered", 10)) // 6
+// console.log(hash("cyan", 10)) // 7
