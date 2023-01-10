d6f6038bee0fd8caaf67e431eb58005a9f1f4a07
Nick Dunn <nick.dunn@flosports.tv>
2023-01-10 09:05:59 -0600

    feat(graph): introduce removeEdge() function

--- graph/index.js
+++ graph/index.js
@@ -18,6 +18,16 @@ class Graph {
     this.adjacencyList[v1].push(v2)
     this.adjacencyList[v2].push(v1)
   }
+
+  /**
+   * Function to remove a connection between two vertices in an undirected graph.
+   * @param v1 vertex1
+   * @param v2 vertex2
+   */
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1)
+  }
 }
 
 const g = new Graph()
@@ -26,4 +36,6 @@ g.addVertex('Dallas')
 g.addVertex('Aspen')
 g.addEdge('Tokyo', 'Dallas')
 g.addEdge('Dallas', 'Aspen')
+console.log(g.adjacencyList)
+g.removeEdge('Tokyo', 'Dallas')
 console.log(g.adjacencyList)
